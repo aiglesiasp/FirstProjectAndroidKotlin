e7decc9c2df712128e031a983a8ba5e943974d63
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-14 13:47:14 +0100

    Dimensiones y Temas

--- app/src/main/java/com/fourpi/firstprojectandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/fourpi/firstprojectandroidkotlin/MainActivity.kt
@@ -2,12 +2,18 @@ package com.fourpi.firstprojectandroidkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var textSaludo = getString(R.string.HelloWorld)
+
+        //Obtener STRINGS
+        val textSaludo = getString(R.string.HelloWorld)
         println(textSaludo)
+
+        //Obtener colores
+        val color = ContextCompat.getColor(this, R.color.orange)
     }
 }
